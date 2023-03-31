c56c28badbee6c97647b23b52695fb47cc92d31a
NicholasNgaruiya <nicholasngaruiya387@gmail.com>
2023-03-31 12:48:27 +0300

    Added a restart Button

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,12 @@ class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
   var _totalScore = 0;
 
+  void _resetQuiz() {
+    setState(() {});
+    _questionIndex = 0;
+    _totalScore = 0;
+  }
+
   void _answerQuestion(int score) {
     _totalScore = _totalScore + score;
     //could also use _totalScore += score;
@@ -74,7 +80,7 @@ class _MyAppState extends State<MyApp> {
                 questionIndex: _questionIndex,
                 questions: _questions,
               )
-            : Result(_totalScore),
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
--- lib/result.dart
+++ lib/result.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
   final int resultScore;
+  final VoidCallback resetHandler;
 
-  const Result(this.resultScore, {super.key});
+  const Result(this.resultScore, this.resetHandler, {super.key});
 
   String get resultPhrase {
     String resultText;
@@ -22,13 +23,23 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        resultPhrase,
-        style: const TextStyle(
-          fontSize: 36,
-          fontWeight: FontWeight.bold,
-        ),
-        textAlign: TextAlign.center,
+      child: Column(
+        children: [
+          Text(
+            resultPhrase,
+            style: const TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          TextButton(
+            onPressed: resetHandler,
+            child: const Text(
+              'Restart Quiz!',
+            ),
+          ),
+        ],
       ),
     );
   }
